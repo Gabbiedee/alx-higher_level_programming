321bfe38af08ef992f201681150a2d37df8999f0
Gabbiedee <akingbojudolly@gmail.com>
2023-05-16 23:05:17 +0100

    Add Errors and Exception files

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <object.h>
+#include <floatobject.h>
+#include <bytesobject.h>
+#include <listobject.h>
+#include <Python.h>
+
+/**
+ * print_python_list - print info on python list
+ * @p: Pointer to p
+ */
+void print_python_list(PyObject *p)
+{
+	size_t a, size, i;
+
+}
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+Learning handling Errors and Exceptions
